90bff1bd91e0c77c7df045661bd606f6242c102a
Saman Zhiani <sampayne69@gmail.com>
2026-07-20 15:02:13 +0330

    add session key to cookie

--- config/middleware.py
+++ config/middleware.py
@@ -1,9 +1,11 @@
-from urllib.parse import urlsplit
-
 from django.contrib import auth
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 
+from .session import SessionStore
+
+
+SESSION_COOKIE_NAME = "sessionid"
 
 def get_user(request):
     if not hasattr(request, "_cached_user"):
@@ -15,22 +17,23 @@ class AuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.user = SimpleLazyObject(lambda: get_user(request))
 
-
-class MySessionMiddleware(MiddlewareMixin):
+class MySessionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         self.process_request(request)
-
         response = self.get_response(request)
 
-        return self.process_response(self, response)
-    
-    def process_request(self, request):
-        pass
+        return self.process_response(request, response)
 
+    def process_request(self, request):
+        session_key = request.COOKIES.get(SESSION_COOKIE_NAME)
+        request.session = SessionStore(session_key)
+        
     def process_response(self, request, response):
+        response.set_cookie(SESSION_COOKIE_NAME, request.session.session_key)
+
         return response
 
 
